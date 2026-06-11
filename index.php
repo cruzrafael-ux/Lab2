17cb796e45980bebc55e0938bcbe4a2ca251806d
RafaelC-tech <143412031+RafaelC-tech@users.noreply.github.com>
2026-06-11 11:49:47 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,21 @@
 <!DOCTYPE html>
-<!--
-Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
-Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
--->
+
 <html>
     <head>
         <meta charset="UTF-8">
         <title></title>
     </head>
     <body>
+        <form action-"index.php" method="get">
+            <label> Pay Rate: </label>
+            <input type="number" name="pay_rate"/><br/>
+            <label> Number of Hours: </label>
+            <input type="number" name="num_hrs"/><br/>
+            <label> Tax Rate: </label>
+            <input type="number" name="tax_rate"/><br/>
+            <input type="submit" value="submit"/>
+        </form>
+        
         <?php
         echo "Hello world from PHP file";
         ?>
